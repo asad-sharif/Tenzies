2ea5238a12de09e7cae24a7ec4fadbd397b715e9
asad-sharif <asadsharif1270@gmail.com>
2025-09-20 17:24:09 +0500

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [number, setNumber] = useState<number>()
+
+  function generateRandomNummber() {
+    const randomNumber: number = Math.ceil(Math.random() * 10)
+    setNumber(randomNumber)
+    console.log(number);
+  }
+
+  return <main>
+    <h1>Tenzies</h1>
+    <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
+    <div id="board">
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((dice, index) => (
+        <div
+          key={index}
+          className={`${(dice === 4 || dice === 7) ? 'dice dice-highlighted' : 'dice'}`}
+        >
+          {dice}
+        </div>
+      ))}
+    </div>
+
+    <button onClick={generateRandomNummber}>Roll</button>
+  </main>
 }
 
 export default App
